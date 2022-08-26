e2e1d7cb14423175f3e333b8a2f6577be3b8ed8a
Daisuke Sato <daisukes@cmu.edu>
2022-08-26 11:05:33 -0400

    bugfix: add --build-arg UID=1000 for docker-compose build

--- launch.sh
+++ launch.sh
@@ -4,6 +4,6 @@ log_prefix=cabot-ble-server
 
 log_name=${log_prefix}_`date +%Y-%m-%d-%H-%M-%S`
 
-docker-compose build && \
+docker-compose build --build-arg UID=$UID && \
 docker-compose up ble | tee log/$log_name.log 1>&2
 
