f67425c66fba1dd50b4d25c24025e13f512a2820
tisihara <tisihara@jp.ibm.com>
2024-12-16 17:23:19 +0900

    fix dockerfile to set host UID, GID, time zone
    DCO 1.1 Signed-off-by: Tatsuya Ishihara <tisihara@jp.ibm.com>

--- docker/ros2/ros_entrypoint.sh
+++ docker/ros2/ros_entrypoint.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 set -e
 
+# Default values
+HOST_UID=${HOST_UID:-1000}
+HOST_GID=${HOST_GID:-1000}
+HOST_TZ=${HOST_TZ:-UTC}
+
+if [[ $TZ != $HOST_TZ ]]; then
+    # Setting up timezone
+    sudo ln -snf /usr/share/zoneinfo/$HOST_TZ /etc/localtime
+    echo $HOST_TZ | sudo tee /etc/timezone
+    export TZ=$HOST_TZ
+fi
+
+CONT_UID=$(id -u developer)
+CONT_GID=$(id -g developer)
+if [[ $CONT_UID -ne $HOST_UID ]] || [[ $CONT_GID -ne $HOST_GID ]]; then
+    # Update user and group ID to match host
+    sudo usermod -u $HOST_UID developer
+    sudo groupmod -g $HOST_GID developer
+fi
+
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
 if [ -f /opt/custom_ws/install/setup.bash ]
@@ -14,4 +34,7 @@ then
 fi
 
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus
-exec "$@"
+
+WORKDIR=$(pwd)
+
+exec gosu developer bash -c "cd $WORKDIR && exec $*"
